e135847b9d0c18e1a5b30d39e0259a9a5c455fa6
RachelY <yang.yijing@outlook.com>
2023-12-05 23:49:52 -0500

    Render data from API on screen

--- app.js
+++ app.js
@@ -15,16 +15,43 @@ function getFloorLayout(floorLevel) {
     fetch(url)
     .then((res) => {return res.json()})
     .then((data) => {
+        renderRoom(data)
         console.log(data)
     })
 }
 
-//function that renter the movie to the dom
-function renderRoomNumber(room){
-    //grab the div.room
-    const room = document.querySelector("")
+function getTheme(id) {
+    //construct our url
+    const url = `${baseURL}/${id}?apikey=${apikey}`
+    //fetch from the url
+    fetch(url)
+    .then((res) => {return res.json()})
+    .then((data2) => {
+        renderDetail(data2)
+        console.log(data2)
+    })
 }
+//function that render the galleryid and name to the dom
+function renderRoom(room){
+    //grab the div.room
+    console.log(room.records)
+    const roomdiv = document.querySelector("ul.roomList")
+    //alter the html inside the div
+    let a = room.records.map((d) => {
+        return `<li>${d.gallerynumber} - ${d.name}</li>`
+    })
+    roomdiv.innerHTML = a
+}
+
+function renderDetail(id){
+    const roomDetails = document.querySelector('.roomD')
+    roomDetails.innerHTML = `
+    <h3>${id.theme} - ${id.labeltext}</h3>
+    `
+}
+
 //*********************** */
 // Main Code
 //************************ */
 getFloorLayout('2')
+getTheme('2100')

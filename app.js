7b29acf57f62036241c7f430f2496f0776009f93
RachelY <yang.yijing@outlook.com>
2023-12-05 21:31:21 -0500

    Build fetch of data in JS file

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-const url = "https://api.harvardartmuseums.org/gallery?apikey=aa3d46c2-16dc-459b-b3dc-77c93007c461"
-fetch(url)
-.then((res) => res.json())
-.then((data) => console.log(data))
+//*********************** */
+// GLOBAL VARIABLES
+//************************ */
+const apikey = "aa3d46c2-16dc-459b-b3dc-77c93007c461"
+const baseURL = "https://api.harvardartmuseums.org/gallery"
+
+//*********************** */
+// Functions
+//************************ */
+// function to fetch the data
+function getFloorLayout(floorLevel) {
+    //construct our url
+    const url = `${baseURL}?floor=${floorLevel}&apikey=${apikey}`
+    //fetch from the url
+    fetch(url)
+    .then((res) => {return res.json()})
+    .then((data) => {
+        console.log(data)
+    })
+}
+
+//function that renter the movie to the dom
+function renderRoomNumber(room){
+    //grab the div.room
+    const room = document.querySelector("")
+}
+//*********************** */
+// Main Code
+//************************ */
+getFloorLayout('2')
